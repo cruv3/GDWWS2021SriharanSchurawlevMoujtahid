22031a682b09b04d07e61d6e9f11e28675af8b4d
moujtahid21 <abdelbassetmoujtahid@smail.th-koeln.de>
2020-12-08 13:40:56 +0100

    Aufgaben begonnen

---
+++ Training/aufgaben_abdelbasset.js
@@ -0,0 +1,15 @@
+//Alle Aufgaben 
+//Namen Ausgeben
+console.log("Abdelbasset");
+
+//Anzahl der Bewertungen
+let i = 0;
+//Bewertung
+const myBewertung = [];
+
+
+const points = x =>{
+    (async function main(){
+        
+    })
+}
